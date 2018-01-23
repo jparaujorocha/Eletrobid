fc8d6b216fde9834ef7754a913262721e404637c
Joao Pedro <jp_rocha1994@yahoo.com.br>
2018-01-23 18:27:51 -0200

    Add project files.

---
+++ Eletrobid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eletrobid.MvcApplication" Language="C#" %>
---
+++ Eletrobid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eletrobid.Startup))]
+namespace Eletrobid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
